eb2153191b686ccd3db5d137d780358eeef9311c
Felipe Limeira <limeira.felipe94@gmail.com>
2024-07-04 03:44:37 -0300

    add grains

---
+++ 21_grains.py
@@ -0,0 +1,14 @@
+def square(number):
+    if number >= 1 and number <= 64:
+        if number == 1:
+            return 1
+        else:
+            return 2 * square(number - 1)
+    raise ValueError("square must be between 1 and 64")
+
+def total():
+    count = 0
+    for i in range(1, 65):
+        count += square(i)
+    return count
+
